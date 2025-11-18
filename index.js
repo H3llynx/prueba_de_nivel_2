931f85f7237d2f858195c62bce1639019bf5d339
Helene T <helene.teychene@gmail.com>
2025-11-18 11:14:04 +0100

    feat/showResult function, and specific styles added depending on result

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { fizzBuzz } from "./src/scripts/fizzbuzz.js";
+import { showResult } from "./src/scripts/ui.js";
 
 document.getElementById("fizzbuzz-form").addEventListener("submit", (e) => {
     e.preventDefault();
     const number = Number(document.getElementById("fizzbuzz-input").value);
-    console.log(fizzBuzz(number));
+    showResult(fizzBuzz(number));
 })
---
+++ src/scripts/ui.js
@@ -0,0 +1,21 @@
+let resultArray = [];
+
+export const showResult = (data) => {
+    const output = document.getElementById("fizzbuzz-output");
+    output.style.visibility = "visible";
+    output.innerHTML = "<h2>Result</h2>";
+    output.innerHTML += `
+    <div class="flex-container">
+    <p>Your input: <b>${data.data.number}</b></p> <p>Result: <span class="result">${data.data.result}</span></p>
+    </div>
+    `;
+    if (data.data.result === "FizzBuzz") {
+        document.querySelector(".result").classList.add("fizzbuzz");
+    }
+    if (data.data.result === "Buzz") {
+        document.querySelector(".result").classList.add("buzz");
+    }
+    if (data.data.result === "Fizz") {
+        document.querySelector(".result").classList.add("fizz");
+    }
+}
